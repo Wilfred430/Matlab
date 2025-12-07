a1bd18b5dc3d8ef3755893e10ed8ea7a988389a5
WenYang <wendeyang2@gmail.com>
2025-12-07 13:10:49 +0800

    turn model into (b)

--- Sliver migration/RC_detection/Transfer_function.m
+++ Sliver migration/RC_detection/Transfer_function.m
@@ -13,8 +13,15 @@ Zpara = parallel(Z1,Zu); % defined Z1 || (Z1 + Zu)
 
 H = (Z1/(Z1+Zu))*(Zpara/(Rs+Zpara));
 
-simplify(H);
+% draw simplified equation
+H_simplified = simplify(H); 
+disp('show the H(s) simplify result：');
+pretty(H_simplified);
 
+% steady-state analysis
+H_steady =limit(H_simplified,s,0);
+disp("show the steady-state analysis : ");
+pretty(H_steady);
 
 % function declaration
 function z = parallel(x,y)
